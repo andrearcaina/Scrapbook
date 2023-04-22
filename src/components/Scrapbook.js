332c7f70a1b0c46157814733f5f8d314ffa89138
Andre <dtandre331@gmail.com>
2023-04-22 17:04:47 -0400

    implemented turn js
    - haven't fully figured out how to get this to work yet
    - i know i need to connect the scrapbook class to app.js which will allow me to see my changes
    - i just need to figure out how to display a book using turn.js

---
+++ src/components/Scrapbook.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import $ from "jquery";
+import "turn.js";
+
+import "./Scrapbook.css";
+
+class Scrapbook extends Component {
+    // haven't figured out how to get this to work yet
+}
+
+export default Scrapbook;
